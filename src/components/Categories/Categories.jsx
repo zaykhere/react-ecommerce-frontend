5160c58c735039469c2da2a5f8827e5794ff91da
zainjhere <zain.javed@softvira.com>
2024-10-03 18:10:40 +0500

    fetch categories

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,89 +1,101 @@
 import React from "react";
 import "./categories.scss";
 import {Link} from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import { Skeleton } from "@mui/material";
 
 const Categories = () => {
+  const {data, loading, error} = useFetch(`/categories?populate=img`);
+
   return (
-    <div className="categories">
-      <div className="col">
-        <div className="row">
-        <img
-            src="https://images.pexels.com/photos/818992/pexels-photo-818992.jpeg?auto=compress&cs=tinysrgb&w=1600"
-            alt=""
-          />
-          <button>
-            <Link className="link" to="/products/1">
-              Sale
-            </Link>
-          </button>
-        </div>
-        <div className="row">
-        <img
-            src="https://images.pexels.com/photos/2036646/pexels-photo-2036646.jpeg?auto=compress&cs=tinysrgb&w=1600"
-            alt=""
-          />
-          <button>
-            <Link to="/products/1" className="link">
-              Women
-            </Link>
-          </button>
-        </div>
-      </div>
-      <div className="col">
-        <div className="row">
-        <img
-            src="https://images.pexels.com/photos/1813947/pexels-photo-1813947.jpeg?auto=compress&cs=tinysrgb&w=1600"
-            alt=""
-          />
-          <button>
-            <Link to="/products/1" className="link">
-              New Season
-            </Link>
-          </button>
-        </div>
-      </div>
-      <div className="col col-l">
-        <div className="row">
-          <div className="col">
-            <div className="row">
-            <img
-                src="https://images.pexels.com/photos/1192609/pexels-photo-1192609.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-              <button>
-                <Link to="/products/1" className="link">
-                  Men
-                </Link>
-              </button>
-            </div>
-          </div>
-          <div className="col">
-            <div className="row">
-            <img
-                src="https://images.pexels.com/photos/2703202/pexels-photo-2703202.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-              <button>
-                <Link to="/products/1" className="link">
-                  Accessories
-                </Link>
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-        <img
-            src="https://images.pexels.com/photos/1159670/pexels-photo-1159670.jpeg?auto=compress&cs=tinysrgb&w=1600"
-            alt=""
-          />
-          <button>
-            <Link to="/products/1" className="link">
-              Shoes
-            </Link>
-          </button>
-        </div>
-      </div>
-    </div>
+    <>
+    {loading && (
+      <Skeleton variant="rectangular" width={1000} height={400} />
+    )}
+    {data && (
+       <div className="categories">
+       <div className="col">
+         <div className="row">
+         <img
+             src={`${import.meta.env.VITE_UPLOAD_URL}${data[0].img.url}`}
+             alt=""
+           />
+           <button>
+             <Link className="link" to={`/products/${data[0].id}`}>
+               {data[0].title}
+             </Link>
+           </button>
+         </div>
+         <div className="row">
+         <img
+             src={`${import.meta.env.VITE_UPLOAD_URL}${data[1].img.url}`}
+             alt=""
+           />
+           <button>
+             <Link to={`/products/${data[1].id}`} className="link">
+             {data[1].title}
+             </Link>
+           </button>
+         </div>
+       </div>
+       <div className="col">
+         <div className="row">
+         <img
+             src={`${import.meta.env.VITE_UPLOAD_URL}${data[2].img.url}`}
+             alt=""
+           />
+           <button>
+             <Link to={`/products/${data[2].id}`} className="link">
+             {data[2].title}
+             </Link>
+           </button>
+         </div>
+       </div>
+       <div className="col col-l">
+         <div className="row">
+           <div className="col">
+             <div className="row">
+             <img
+                 src={`${import.meta.env.VITE_UPLOAD_URL}${data[3].img.url}`}
+                 alt=""
+               />
+               <button>
+                 <Link to={`/products/${data[3].id}`} className="link">
+                  {data[3].title}
+                 </Link>
+               </button>
+             </div>
+           </div>
+           <div className="col">
+             <div className="row">
+             <img
+                 src={`${import.meta.env.VITE_UPLOAD_URL}${data[4].img.url}`}
+                 alt=""
+               />
+               <button>
+                 <Link to={`/products/${data[4].id}`} className="link">
+                  {data[4].title}
+                 </Link>
+               </button>
+             </div>
+           </div>
+         </div>
+         <div className="row">
+         <img
+             src={`${import.meta.env.VITE_UPLOAD_URL}${data[5].img.url}`}
+             alt=""
+           />
+           <button>
+             <Link to={`/products/${data[5].id}`} className="link">
+              {data[5].title}
+             </Link>
+           </button>
+         </div>
+       </div>
+     </div>
+    )}
+   
+    </>
   );
 };
 
